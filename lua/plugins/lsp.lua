3107334b1f915d7403002ee19d43a58675757233
Keegan Conlee <dacid44@gmail.com>
2024-05-26 19:29:04 -0500

    add clangd LSP

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -11,7 +11,12 @@ return {
               }
             },
           }
-        }
+        },
+        clangd = {
+          capabilities = {
+            offsetEncoding = "utf-8",
+          },
+        },
       },
       taplo = {
         keys = {
